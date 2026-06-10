37d37d3cb324f14186b3c405b8ebbb8c66dd75d3
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-06-10 15:25:19 +0530

    DB connection

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.use(cors({credentials: true}), express.json(), express.urlencoded({extended: true}));
--- services/auth/src/model/User.ts
+++ services/auth/src/model/User.ts
@@ -1,14 +1,15 @@
 import mongoose, {Document,Schema} from "mongoose";
 
 
-export interface IUser extends Document{
+export interface IUser extends Document{ // it tells -> Every User document contains these fields.
+    // why extends Document? -> because we want to use the features of mongoose document like save(), find(), etc.
     name: string;
     email: string;
     image: string;
     role: string;
 }
 
-const schema : Schema<IUser> = new Schema({
+const schema : Schema<IUser> = new Schema({ // Schema<IUser>  -->> This schema should follow the IUser interface.
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     image: {type: String, required: true},
