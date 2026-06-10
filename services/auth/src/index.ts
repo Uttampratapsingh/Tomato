5fd25af7c58e1b50432718cae013efb608fa6033
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-06-10 15:11:11 +0530

    Setup the basic server with typescript

---
+++ services/auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({credentials: true}), express.json(), express.urlencoded({extended: true}));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Auth service is running' });
+});
+
+app.listen(PORT,()=>{
+    console.log(`Auth service is running on port ${PORT}`)
+})
